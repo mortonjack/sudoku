5ebf677581fe536d78997f387de48dbc08303340
mortonjack <morton.jack@protonmail.com>
2023-01-13 16:03:33 +1030

    fill notes refactor

--- app/src/main/java/com/example/sudoku/game/Board.kt
+++ app/src/main/java/com/example/sudoku/game/Board.kt
@@ -37,33 +37,39 @@ class Board(private val size: Int) {
         return grid[row][col].value
     }
 
+    private fun fillNotes(row: Int, col: Int) {
+        // Store possible values this cell could take
+        var possible = Array<Boolean>(size) { _ -> true }
+
+        // Check row & col to remove possible values
+        for (i in 0 until size) {
+            val rNum = cellValue(row, i)
+            val cNum = cellValue(i, col)
+            if (rNum > 0) possible[rNum-1] = false
+            if (cNum > 0) possible[cNum-1] = false
+        }
+
+        // Check block to remove possible values
+        for (i in 3*(row/3) until 3 *((row/3)+1)) {
+            for (j in 3*(col/3) until 3*((col/3)+1)) {
+                val num = cellValue(i, j)
+                if (num > 0) possible[num-1] = false
+            }
+        }
+
+        // Fill notes for possible values
+        for (i in 0 until size) {
+            if (possible[i] && !isNote(row, col, i+1)) {
+                grid[row][col].flipNote(i+1)
+            }
+        }
+    }
+
     fun fillAllNotes() {
-        for (r in 0 until size) {
-            for (c in 0 until size) {
-                if (cellValue(r, c) <= 0) {
-                    // Fill in notes for possible values this could take
-                    var possible = Array<Boolean>(size) { _ -> true }
-                    // Check row/col
-                    for (i in 0 until size) {
-                        val rNum = cellValue(r, i)
-                        val cNum = cellValue(i, c)
-                        if (rNum > 0) possible[rNum-1] = false
-                        if (cNum > 0) possible[cNum-1] = false
-                    }
-                    // Check block
-                    for (i in 3*(r/3) until 3 *((r/3)+1)) {
-                        for (j in 3*(c/3) until 3*((c/3)+1)) {
-                            val num = cellValue(i, j)
-                            if (num > 0) possible[num-1] = false
-                        }
-                    }
-                    // Fill notes for possible values
-                    for (i in 0 until size) {
-                        if (possible[i] && !isNote(r, c, i+1)) {
-                            grid[r][c].flipNote(i+1)
-                        }
-                    }
-                }
+        for (row in 0 until size) {
+            for (col in 0 until size) {
+                // Fill notes for all cells without a value
+                if (cellValue(row, col) > 0) fillNotes(row, col)
             }
         }
     }
